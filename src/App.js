033c0fa0be2d536bda4c962913c985ee6c08255b
A01753505 <A01753505@tec.mx>
2024-06-07 17:07:59 -0600

    Cambios 1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Connect from './componentes/Connect';
+
+function App() {
+  return (
+    <div className="App">
+      <Connect/>
+    </div>
+  );
+}
+
+export default App;
